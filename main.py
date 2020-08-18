e94a683bf39133fbe76a0fe8e9806692a19c68c1
erperez <35548849+erperez@users.noreply.github.com>
2020-08-18 09:45:03 -0400

    Refactored into functions

--- main.py
+++ main.py
@@ -20,39 +20,56 @@ movie_data = pd.read_csv("test_clean_maybe.csv")
 
 # Starts the connection to imdb
 im = IMDb()
+
+
+def imdb_query():
+    global imdb_date
+    imdb_date = []
+    # Uses list comprehension to extract the imdb_id from the dataframe into a stand-alone list
+    imdb_list = [imdb_id for imdb_id in movie_data['imdb_id']]
+    #
+    #
+    # This loop will grab the imdb_id from our list called `imdb_list` after getting the idea it'll strip out the beginning
+    # 2 characters leaving only the number part
+    for movie_id in imdb_list:
+        # this will query the imdb website to get all the information based on that movie id
+        # such as cast, genres, runtimes, year it was released among others
+        # does not include budget
+        imdb_information = (im.get_movie(movie_id[2:]))
+        print(str(movie_id) + " " + str(imdb_information.data['year']) + " " + imdb_information.data['title'])
+        # in this for loop you can get different information from what the api is returning.
+        # should be able to access any column by doing imdb_information.data['name of column here']
+
+        imdb_date.append(imdb_information.data['year'])
+        return imdb_date
+
+
 # Creating a dictionary (unique key : value pair) to associate a movie id with its release date to make sure there is no
 # data mis-match
-# imdb_date = []
-#
-#
-# # Uses list comprehension to extract the imdb_id from the dataframe into a stand-alone list
-# imdb_list = [imdb_id for imdb_id in movie_data['imdb_id']]
-# #
-# #
-# # This loop will grab the imdb_id from our list called `imdb_list` after getting the idea it'll strip out the beginning
-# # 2 characters leaving only the number part
-# for movie_id in imdb_list:
-#     # this will query the imdb website to get all the information based on that movie id
-#     # such as cast, genres, runtimes, year it was released among others
-#     # does not include budget
-#     imdb_information = (im.get_movie(movie_id[2:]))
-#     print(str(movie_id) + " " + str(imdb_information.data['year']) +  " " + imdb_information.data['title'])
-#     # in this for loop you can get different information from what the api is returning.
-#     # should be able to access any column by doing imdb_information.data['name of column here']
-#
-#     imdb_date.append(imdb_information.data['year'])
+#imdb_query()
 
-# t = pd.DataFrame(imdb_date)
-# t.to_csv('imdb_date.csv')
-test = [release for release in movie_data['release_date']]
-imdb_date = pd.read_csv('imdb_date.csv')
-corrected_date = []
-year = [imdb_year for imdb_year in imdb_date['year']]
-print("creating new date field")
-for (i, z) in zip(test, year):
-    print(i[:-4] + str(z))
-    corrected_date.append(str(i[:-4]) + str(z))
 
-movie_data['cleaned_year'] = corrected_date
+def clean_dates():
+    global imdb_date, corrected_date
+    test = [release for release in movie_data['release_date']]
+    imdb_date = pd.read_csv('imdb_date.csv')
+    corrected_date = []
+    year = [imdb_year for imdb_year in imdb_date['year']]
+    print("creating new date field")
+    for (i, z) in zip(test, year):
+        print(i[:-4] + str(z))
+        corrected_date.append(str(i[:-4]) + str(z))
 
-movie_data.to_csv("cleaned_test.csv")
+
+#
+# clean_dates()
+#
+# movie_data['cleaned_year'] = corrected_date
+#
+# movie_data.to_csv("cleaned_test.csv")
+
+
+"""
+Next step is to clean up budget to remove all low budgets that are at 0
+using the tmdb API 
+"""
